9a4918fa92954bba168ad7da4fe1c774124eedf9
wissegt <wissegt@DESKTOP-RGA1D99>
2024-10-09 13:17:07 -0600

    actualizcion MEnu Evaluacion

--- NotasAlumnosFX/src/Controlador/MenuEvaluacionController.java
+++ NotasAlumnosFX/src/Controlador/MenuEvaluacionController.java
@@ -7,7 +7,15 @@ package Controlador;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 /**
  * FXML Controller class
@@ -16,6 +24,31 @@ import javafx.fxml.Initializable;
  */
 public class MenuEvaluacionController implements Initializable {
 
+    @FXML
+    private ChoiceBox<?> conCurso;
+    @FXML
+    private ChoiceBox<?> conBimestre;
+    @FXML
+    private Label rGrado;
+    @FXML
+    private Label rSeccion;
+    @FXML
+    private ChoiceBox<?> conTipo;
+    @FXML
+    private TextField txtNombre;
+    @FXML
+    private TextField txtPonderacion;
+    @FXML
+    private TableView<?> tblPersona;
+    @FXML
+    private TableColumn<?, ?> EvaluacionId;
+    @FXML
+    private TableColumn<?, ?> nombreActividad;
+    @FXML
+    private TableColumn<?, ?> tipoActividad;
+    @FXML
+    private TableColumn<?, ?> ponderacionActividad;
+
     /**
      * Initializes the controller class.
      */
@@ -23,5 +56,21 @@ public class MenuEvaluacionController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void btnGuardar(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnActualizar(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnListar(MouseEvent event) {
+    }
+
+    @FXML
+    private void seleccionaModificar(MouseEvent event) {
+    }
     
 }
