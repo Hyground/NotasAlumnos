c6d119df7b08c2a4a791cb5d31dd5e0d0dcf8ee7
PC <PC@LAPTOP-52AN4T04>
2024-10-09 23:28:41 -0600

    Menu Inscripcion makeee

--- NotasAlumnosFX/src/Controlador/MenuInscripsionController.java
+++ NotasAlumnosFX/src/Controlador/MenuInscripsionController.java
@@ -7,7 +7,12 @@ package Controlador;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -16,6 +21,23 @@ import javafx.fxml.Initializable;
  */
 public class MenuInscripsionController implements Initializable {
 
+    @FXML
+    private TextField txtCodigoPersonal;
+    @FXML
+    private TextField txtCui;
+    @FXML
+    private TextField txtApellido;
+    @FXML
+    private TextField txtNombre;
+    @FXML
+    private Button btnGuardar;
+    @FXML
+    private Button btnEliminar;
+    @FXML
+    private Button btnModificar;
+    @FXML
+    private TableColumn<?, ?> tblInscripcionEstudiantes;
+
     /**
      * Initializes the controller class.
      */
@@ -23,5 +45,17 @@ public class MenuInscripsionController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void guardar(ActionEvent event) {
+    }
+
+    @FXML
+    private void eliminar(ActionEvent event) {
+    }
+
+    @FXML
+    private void modificar(ActionEvent event) {
+    }
     
 }
