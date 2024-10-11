cc3c40bf05a92a50e55e7c41b4ae0d763dcd8763
PC <PC@LAPTOP-52AN4T04>
2024-10-10 21:12:06 -0600

    Menu Notass2

--- NotasAlumnosFX/src/Controlador/MenuNotasController.java
+++ NotasAlumnosFX/src/Controlador/MenuNotasController.java
@@ -7,7 +7,13 @@ package Controlador;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -16,6 +22,25 @@ import javafx.fxml.Initializable;
  */
 public class MenuNotasController implements Initializable {
 
+    @FXML
+    private TableView<?> tblNotas;
+    @FXML
+    private Button btnModificar;
+    @FXML
+    private Button btnEliminar;
+    @FXML
+    private Button btnGuardar;
+    @FXML
+    private TextField txtNombre;
+    @FXML
+    private TextField txtApellido;
+    @FXML
+    private ChoiceBox<?> chCurso;
+    @FXML
+    private ChoiceBox<?> chActividad;
+    @FXML
+    private TextField txtNota;
+
     /**
      * Initializes the controller class.
      */
@@ -23,5 +48,17 @@ public class MenuNotasController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void modificar(ActionEvent event) {
+    }
+
+    @FXML
+    private void eliminar(ActionEvent event) {
+    }
+
+    @FXML
+    private void guardar(ActionEvent event) {
+    }
     
 }
