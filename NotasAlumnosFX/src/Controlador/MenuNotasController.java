0b950e02f862330f2833e950dda10edc4fa23a51
vicen <vicen@IngMigue>
2024-10-20 14:25:49 -0600

    ya casi

--- NotasAlumnosFX/src/Controlador/MenuNotasController.java
+++ NotasAlumnosFX/src/Controlador/MenuNotasController.java
@@ -1,45 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Controlador;
+
 import CRUDs.CBimestres;
 import CRUDs.CCurso;
 import CRUDs.CEvaluaciones;
 import POJOs.Cursos;
 import POJOs.Evaluaciones;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import Modelo.TablaNotas;
-import java.util.List;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
+import POJOs.Bimestres;
 
 /**
  * FXML Controller class
  *
- * @author wissegt
  */
 public class MenuNotasController implements Initializable {
 
     @FXML
     private TableView<TablaNotas> tblNotas;
-    @FXML
-    private Button btnModificar;
-    @FXML
-    private Button btnEliminar;
-    @FXML
-    private Button btnGuardar;
     @FXML
     private TextField txtNombre;
     @FXML
@@ -50,75 +39,88 @@ public class MenuNotasController implements Initializable {
     private ChoiceBox<String> chActividad;
     @FXML
     private TextField txtNota;
-    ////////////////////////////////////////////////////
-    /////// ya no me lo vuelvan a eliminar me sirve////
-   /*/////////////*/ private Integer gradoId;/////////
-   /*/////////////*/ private Integer seccionId;//////
-    ////////////////////////////////////////////////
     @FXML
     private Label rGrado;
     @FXML
     private Label rSeccion;
     @FXML
-    private TableColumn<TablaNotas, ?> tbNombre;
+    private TableColumn<TablaNotas, String> tbNombre;
     @FXML
-    private TableColumn<TablaNotas, ?> tbActividad;
+    private TableColumn<TablaNotas, String> tbActividad;
     @FXML
-    private TableColumn<TablaNotas, ?> tbPunto;
+    private TableColumn<TablaNotas, String> tbPunto;
     @FXML
-    private TableColumn<TablaNotas, ?> tbApellido;
+    private TableColumn<TablaNotas, String> tbApellido;
+    @FXML
+    private ChoiceBox<String> conBimestre;
 
-    /**
-     * Initializes the controller class.
-     */
+    // Almacenamos el gradoId y seccionId que vienen del MenuEvaluacionController
+    private Integer gradoId;
+    private Integer seccionId;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        // Inicializamos los datos del ChoiceBox con base en las evaluaciones
         cargarCurso();
-      mostrar();
-        // TODO
-    }  
-    private void mostrar() {
-        
-        
-        
+        cargarActividades();
+        cargarBimestres();
     }
-    
-    
-    
-        public  void setDatosGradoSeccion (String grado, String seccion, Integer gradoId, Integer seccionId){
-           this.gradoId = gradoId;       // Almacenar el ID del grado
-        this.seccionId = seccionId;   // Almacenar el ID de la sección
-        rGrado.setText(grado);      // Mostrar el nombre del grado
-        rSeccion.setText(seccion);  // Mostrar el nombre de la sección
-        
-    }
-        public void cargarCurso(){
-            List<Cursos> listtaCurso = CCurso.listarCursos();
-            ObservableList<String> curso = FXCollections.observableArrayList();
-            for (Cursos cursos: listtaCurso) {
-                curso.add(cursos.getNombreCurso());
-            }
-            chCurso.setItems(curso);
-}
-                public void cargarActividades(){
-            List<Evaluaciones> listtaEvaluaciones = CEvaluaciones.universo(gradoId, seccionId);
-            ObservableList<String> evaluacion = FXCollections.observableArrayList();
-            for (Evaluaciones Evaluaciones: listtaEvaluaciones) {
-                evaluacion.add(Evaluaciones.getNombreEvaluacion());
-            }
-            chActividad.setItems(evaluacion);
-}
 
+    // Método para recibir los datos del grado y la sección desde el controlador anterior
+    public void setDatosGradoSeccion(String grado, String seccion, Integer gradoId, Integer seccionId) {
+        this.gradoId = gradoId;
+        this.seccionId = seccionId;
+        rGrado.setText(grado);  // Mostramos el nombre del grado
+        rSeccion.setText(seccion);  // Mostramos el nombre de la sección
+        cargarCurso();  // Recargamos los cursos filtrados por grado
+        cargarActividades();  // Recargamos las actividades (evaluaciones) filtradas
+        cargarBimestres();  // Recargamos los bimestres
+    }
+
+    // Cargamos los cursos filtrados por grado
+    private void cargarCurso() {
+        if (gradoId != null) {
+            List<Cursos> listaCursos = CCurso.listarCursosPorGrado(gradoId);
+            ObservableList<String> cursos = FXCollections.observableArrayList();
+            listaCursos.forEach(curso -> cursos.add(curso.getNombreCurso()));
+            chCurso.setItems(cursos);
+            chCurso.setValue("Selecciona un curso");
+        }
+    }
+
+    // Cargamos las actividades (evaluaciones) filtradas por grado y sección
+    private void cargarActividades() {
+        if (gradoId != null && seccionId != null) {
+            List<Evaluaciones> listaEvaluaciones = CEvaluaciones.universo(gradoId, seccionId);
+            ObservableList<String> actividades = FXCollections.observableArrayList();
+            listaEvaluaciones.forEach(evaluacion -> actividades.add(evaluacion.getNombreEvaluacion()));
+            chActividad.setItems(actividades);
+            chActividad.setValue("Selecciona una actividad");
+        }
+    }
+
+    // Cargamos los bimestres disponibles
+    private void cargarBimestres() {
+        List<Bimestres> listaBimestres = CBimestres.listarBimestres();
+        ObservableList<String> bimestres = FXCollections.observableArrayList();
+        listaBimestres.forEach(bimestre -> bimestres.add(bimestre.getNombreBimestre()));
+        conBimestre.setItems(bimestres);
+        conBimestre.setValue("Selecciona un bimestre");
+    }
+
+    // Aquí puedes añadir los métodos para modificar, eliminar y guardar las notas si lo deseas.
     @FXML
     private void modificar(ActionEvent event) {
+        // Código para modificar
     }
 
     @FXML
     private void eliminar(ActionEvent event) {
+        // Código para eliminar
     }
 
     @FXML
     private void guardar(ActionEvent event) {
+        // Código para guardar
     }
-    
 }
